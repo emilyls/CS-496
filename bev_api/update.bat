cb2c2d7cc911ba5aca639565f65b8252a9397e73
emilyls <snyderem@oregonstate.edu>
2016-07-14 23:14:12 -0700

    added script to deploy

---
+++ bev_api/update.bat
@@ -0,0 +1,2 @@
+C:\Python27\python.exe "C:\Program Files (x86)\Google\google_appengine\appcfg.py" -V 1 update .
+C:\Python27\python.exe "C:\Program Files (x86)\Google\google_appengine\appcfg.py" set_default_version .
